159c09eb088dc24cbbdecd349a8a879df26a3b3c
Fixelx <69038808+Fixelx@users.noreply.github.com>
2024-07-18 03:23:47 +0200

    Updated

--- module/picture.py
+++ module/picture.py
@@ -1,10 +1,15 @@
-from time import sleep
-from picamera2 import Picamera2, Preview
 import os
+from time import sleep
+from picamera2 import Picamera2
 
 def take():
-    name="test.jpg"
+    directory = "img"
+    name = os.path.join(directory, "object.jpg")
     picam2 = Picamera2()
-    picam2.start_and_capture_file(name)
-    picam2.close()
-    return True
+    try:
+        picam2.start_preview()
+        picam2.start_and_capture_file(name)
+        return True
+    finally:
+        picam2.stop_preview()
+        picam2.close()

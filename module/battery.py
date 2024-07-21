6e536aadec68c5df766254262788ee1df080e01f
Fixelx <69038808+Fixelx@users.noreply.github.com>
2024-07-21 19:15:25 +0200

    Update Website

--- module/battery.py
+++ module/battery.py
@@ -1,26 +1,22 @@
 import Adafruit_ADS1x15
 
-# Create an ADS1115 ADC (analog-to-digital converter) instance on I2C bus 1
 adc = Adafruit_ADS1x15.ADS1115(busnum=1)
 
-# Gain setting, 1x means +-4.096V
 GAIN = 1
 
-# Voltage range for the battery (adjust according to your battery type)
-# Example for Alkaline batteries:
 VOLTAGE_MIN = 0.8
 VOLTAGE_MAX = 1.5
+NO_CONNECTION_THRESHOLD = 0.1  # Schwellenwert für die Erkennung von keiner Verbindung
 
 def read_voltage(channel):
-    # Read the value from the specified channel
     value = adc.read_adc(channel, gain=GAIN)
-    # Convert the reading to voltage
     voltage = value * 4.096 / 32767
     return voltage
 
 def calculate_percentage(voltage):
-    # Calculate the percentage based on the voltage range
-    if voltage < VOLTAGE_MIN:
+    if voltage < NO_CONNECTION_THRESHOLD:
+        return None  # Kein angeschlossenes Gerät erkannt
+    elif voltage < VOLTAGE_MIN:
         return 0
     elif voltage > VOLTAGE_MAX:
         return 100
@@ -35,6 +31,6 @@ def get_battery_status():
         percentage = calculate_percentage(voltage)
         battery_status[f'battery{channel + 1}'] = {
             'voltage': round(voltage, 2),
-            'percentage': round(percentage, 2)
+            'percentage': percentage
         }
     return battery_status
